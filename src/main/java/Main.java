8bff6279660995b736cabd15daaf3c48d77b7dba
Sumit Gupta <131431222+Sumitgupta22@users.noreply.github.com>
2024-04-06 21:09:26 +0530

    update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,21 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
+class Demo{
+  public static void printnumber(int a){
+    //base condition 
+    if(a==0){
+      return;
+    }
+   printnumber(a-1);
 
+ }
+}
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
+    Demo obj = Demo()
+    obj.printnumber(10);
   }
 
   // @Test
